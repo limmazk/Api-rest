abd4ec91ec7bbb9bc21e054a104d028297996482
Arthur Lima <zklimma07@gmail.com>
2026-08-18 13:18:41 -0300

    Utilizando a dependencia BCrypt e gerando senha criptografada

--- src/main/java/br/com/limmazk/todolist/user/UserController.java
+++ src/main/java/br/com/limmazk/todolist/user/UserController.java
@@ -1,5 +1,6 @@
 package br.com.limmazk.todolist.user;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,7 +29,12 @@ public class UserController {
                     .body("Usuario ja existe");
        }
 
-       var userCreated = this.userRepository.save(userModel);
+       var passwordHashred = BCrypt.withDefaults()
+               .hashToString(12, userModel.getPassword().toCharArray());
+
+       userModel.setPassword(passwordHashred);
+
+       this.userRepository.save(userModel);
        return ResponseEntity
                .status(HttpStatus.CREATED)
                .body("Usuario criado com sucesso");
