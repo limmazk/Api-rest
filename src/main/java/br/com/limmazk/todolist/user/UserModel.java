3464c6ae90bd1ba59c25504b9a0e22fa9d25a683
Arthur Lima <zklimma07@gmail.com>
2026-08-17 22:56:15 -0300

    Criação do metodo Post e do Repository

--- src/main/java/br/com/limmazk/todolist/user/UserModel.java
+++ src/main/java/br/com/limmazk/todolist/user/UserModel.java
@@ -1,8 +1,25 @@
 package br.com.limmazk.todolist.user;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity(name = "tb_users")
 public class UserModel {
-    
-    String username;
-    String name;
-    String password;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(unique = true)
+   private String username;
+   private String name;
+   private String password;
+
+   @CreationTimestamp
+   private LocalDateTime createdAt;
 }
---
+++ src/main/java/br/com/limmazk/todolist/user/UserRepository.java
@@ -0,0 +1,11 @@
+package br.com.limmazk.todolist.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<UserModel, UUID> {
+
+    UserModel findByUsername(String username);
+
+}
